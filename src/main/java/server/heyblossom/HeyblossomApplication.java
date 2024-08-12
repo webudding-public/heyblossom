e48ece1c2d00a890277aeceb4e3896d4d0ab1959
gyeongminK <sally_k@naver.com>
2024-08-13 00:57:38 +0900

    feat: init

---
+++ src/main/java/server/heyblossom/HeyblossomApplication.java
@@ -0,0 +1,13 @@
+package server.heyblossom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyblossomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyblossomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/heyblossom/HeyblossomApplicationTests.java
@@ -0,0 +1,13 @@
+package server.heyblossom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyblossomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
