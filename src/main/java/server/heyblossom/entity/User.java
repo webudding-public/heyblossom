b9f128f4cae5c2169a28054e4cecbe7195b0fd59
gyeongminK <sally_k@naver.com>
2024-08-13 05:08:29 +0900

    feat: postgresql 세팅

---
+++ src/main/java/server/heyblossom/entity/User.java
@@ -0,0 +1,36 @@
+package server.heyblossom.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Table(name = "Users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false )
+    @ColumnDefault("0")
+    private Integer givenCount;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Integer receivedCount;
+}
