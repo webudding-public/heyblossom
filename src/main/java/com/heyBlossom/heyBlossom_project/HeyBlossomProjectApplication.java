10ace4aef66f67a8207ca5196255a2e19f416fcb
HugeKyung <kiki9510@gmail.com>
2024-08-14 01:05:08 +0900

    feat: springboot 초기 세팅

---
+++ src/main/java/com/heyBlossom/heyBlossom_project/HeyBlossomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.heyBlossom.heyBlossom_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyBlossomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyBlossomProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heyBlossom/heyBlossom_project/HeyBlossomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyBlossom.heyBlossom_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyBlossomProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
