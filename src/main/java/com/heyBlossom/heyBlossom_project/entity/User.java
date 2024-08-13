b3f07df4824b6b4eea85869c08473482d1007646
HugeKyung <kiki9510@gmail.com>
2024-08-14 01:52:28 +0900

    feat: User entity 추가

---
+++ src/main/java/com/heyBlossom/heyBlossom_project/entity/User.java
@@ -0,0 +1,11 @@
+package com.heyBlossom.heyBlossom_project.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
